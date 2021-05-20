9b7f9d676f4c3e68e16d1bdfe2d628185ed78475
chandu <chandrikarani005@gmail.com>
2021-05-20 09:27:28 +0530

    deleted hostelMS

---
+++ HostelMS/src/main/java/com/mph/controller/ComplaintsRestController.java
@@ -0,0 +1,63 @@
+package com.mph.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mph.entity.Complaints;
+import com.mph.service.ComplaintsService;
+
+@RestController
+public class ComplaintsRestController {
+	@Autowired
+	ComplaintsService complaintsService;
+	
+	@GetMapping("/allcomplaints")
+	private ResponseEntity<List<Complaints>> allComplaints(){
+		List<Complaints> clist=complaintsService.getComplaintsList();
+		
+		System.out.println("complaints list: "+clist);
+		if(clist.isEmpty()) {
+			return new ResponseEntity<List<Complaints>>(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<List<Complaints>>(clist,HttpStatus.OK);
+	}
+	
+	@PostMapping("/createcomplaint")
+	private Complaints createEmployee(@RequestBody Complaints complaint){
+		
+		complaintsService.createComplaint(complaint);
+		return complaint;
+		
+	}
+	@DeleteMapping("/deletecomplaint/{txtcid}")
+	private ResponseEntity<List<Complaints>> deleteComplaint(@PathVariable("txtcid") int complaint_id){
+		
+		List<Complaints> list=complaintsService.deleteComplaint(complaint_id);
+		System.out.println("complaint list: "+list);
+		if(list.isEmpty()) {
+			return new ResponseEntity<List<Complaints>>(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<List<Complaints>>(list,HttpStatus.OK);
+	}
+	
+	@PutMapping("/updatecomplaint")
+	private ResponseEntity<List<Complaints>> updateComplaint(@RequestBody Complaints complaint){
+		
+		List<Complaints> list=complaintsService.updateComplaint(complaint);
+		System.out.println("complaint list: "+list);
+		if(list.isEmpty()) {
+			return new ResponseEntity<List<Complaints>>(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<List<Complaints>>(list,HttpStatus.OK);
+	}
+}
---
+++ HostelMS/src/main/java/com/mph/service/ComplaintsServiceImpl.java
@@ -0,0 +1,49 @@
+package com.mph.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.mph.dao.ComplaintsDao;
+import com.mph.entity.Complaints;
+
+@Service
+@Transactional
+public class ComplaintsServiceImpl implements ComplaintsService{
+	
+	@Autowired
+	ComplaintsDao complaintsDao;
+	
+	@Override
+	public void createComplaint(Complaints complaint) {
+	complaintsDao.createComplaint(complaint);
+		
+	}
+
+	@Override
+	public List<Complaints> getComplaintsList() {
+		return 	complaintsDao.getComplaintsList();
+	
+	}
+
+	@Override
+	public Complaints getComplaint(Complaints complaint) {
+		return 	complaintsDao.getComplaint(complaint);
+		
+	}
+
+	@Override
+	public List<Complaints> updateComplaint(Complaints complaint) {
+		return 	complaintsDao.updateComplaint(complaint);
+		
+	}
+
+	@Override
+	public List<Complaints> deleteComplaint(int complaint_id) {
+		return complaintsDao.deleteComplaint(complaint_id);
+		
+	}
+
+}
